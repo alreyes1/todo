f4e9ba813d351033a072648a94360c3a71daf624
alreyes1 <alreyes1@icloud.com>
2015-05-16 14:04:05 -0400

    migrated to use postgresql

--- Gemfile
+++ Gemfile
@@ -22,10 +22,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem 'puma'
 group :production do
   gem 'rails_12factor'
-  gem 'puma'
 end
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150515182132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
     t.integer  "todo_list_id"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150515182132) do
     t.datetime "completed_at"
   end
 
-  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "tittle"
