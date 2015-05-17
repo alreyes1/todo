fdf50bcaeb0e3f50b6d6a389c2738bbdf283f5d8
alreyes1 <alreyes1@icloud.com>
2015-05-17 18:10:22 -0400

    Revert changes to schema

--- db/migrate/20150513230209_create_todo_lists.rb
+++ db/migrate/20150513230209_create_todo_lists.rb
@@ -1,7 +1,7 @@
 class CreateTodoLists < ActiveRecord::Migration
   def change
     create_table :todo_lists do |t|
-      t.string :title
+      t.string :tittle
       t.text :description
 
       t.timestamps null: false
